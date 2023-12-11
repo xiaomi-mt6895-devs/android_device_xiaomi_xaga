46dfc159f1e688f9580d6ff54e4bc747f124c14d
Giovanni Ricca <giovanniricca@proton.me>
2023-12-11 23:17:53 +0100

    xaga: Setup kernel modules
    * modules.load files has been extracted from V14.0.6.0.TLOMIXM
    
    Change-Id: I24e14fe63b70a3059c3799523ee216677cecd445

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,5 +9,11 @@ DEVICE_PATH := device/xiaomi/xaga
 # Inherit from mt6895-common
 include device/xiaomi/mt6895-common/BoardConfigCommon.mk
 
+# Kernel
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.vendor_boot))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD) $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD)
+
 # Display
 TARGET_SCREEN_DENSITY := 480
