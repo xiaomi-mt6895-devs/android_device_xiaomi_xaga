9958c6ef00f3e1f0ee665e3ce8fe1b334b19fb85
Giovanni Ricca <giovanniricca@proton.me>
2023-12-11 14:49:53 +0100

    xaga: overlay: Import power profile from V14.0.6.0.TLOMIXM
    Change-Id: I9ce4863a6e25f9cb806128c96ffda1fabe15b92a

--- device.mk
+++ device.mk
@@ -7,6 +7,12 @@
 # Inherit from mt6895-common
 $(call inherit-product, device/xiaomi/mt6895-common/mt6895.mk)
 
+# Overlay
+PRODUCT_PACKAGES += \
+    FrameworksResXaga \
+    FrameworksResXagaPro \
+    FrameworksResXagaProIn
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
