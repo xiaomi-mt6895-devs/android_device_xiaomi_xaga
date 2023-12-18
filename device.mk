2acaaee5fe43a8d2e34cf62810b9c99c6764fc0c
Giovanni Ricca <giovanniricca@proton.me>
2023-12-18 18:31:28 +0100

    xaga: Setup Sensors stack
    Change-Id: I57de0574379272f9ad62601d710a7a85229a36f6

--- device.mk
+++ device.mk
@@ -54,6 +54,10 @@ $(foreach sku, xaga xagapro, \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.xaga.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.xaga.rc
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 31
 
